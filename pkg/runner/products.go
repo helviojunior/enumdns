dce829c21703c44685b3d5fda4a815c70307faed
Helvio Junior <helvio_junior@hotmail.com>
2025-04-22 22:56:23 -0300

    Add products

--- pkg/runner/products.go
+++ pkg/runner/products.go
@@ -3,7 +3,7 @@ package runner
 var products = map[string][]string{
     "CloudFront": []string{ "cloudfront.net", "cloudfront", },
     "CloudFlare": []string{ "cloudflare.com", "cloudflare", "cc-ecdn.net" },
-    "Akamai": []string{ "akamaitechnologies.com", "edgekey.net", "akamaiedge.net" },
+    "Akamai": []string{ "akamaitechnologies.com", "edgekey.net", "akamaiedge.net", "akam.net" },
     "Imperva": []string{ "incapsula.com" },
     "Sucuri": []string{ "sucuri.net" },
     "Bunny": []string{ "bunnyinfra.net" },
@@ -12,10 +12,12 @@ var products = map[string][]string{
     "AWS Global Accelerator": []string{ "awsglobalaccelerator.com", },
     "AWS": []string{ "amazonaws.com", },
     "Microsoft Office 365": []string{ "lync.com", "office.com", "outlook.com" },
-    "Azure": []string{ "azure-dns.com", "azure-dns.net", "azure-dns.org", "azure-dns.info" },
+    "Microsoft Sharepoint": []string{ "sharepointonline.com" },
+    "Azure": []string{ "azure-dns.com", "azure-dns.net", "azure-dns.org", "azure-dns.info", "azurewebsites.net" },
     "Locaweb": []string{ "locaweb.com.br" },
     "Oracle Cloud": []string{ "oraclecloud.net" },
     "GCP": []string{ "googleusercontent.com" },
     "Heroku": []string{ "herokuapp.com", "herokudns.com" },
     "Registro.BR": []string { "dns.br" },
+    "Trend Micro Email Security": []string { "tmes.trendmicro.com" },
 }
